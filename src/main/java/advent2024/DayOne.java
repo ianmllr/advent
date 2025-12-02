44c216ce3c20bb566583571bab480940bd56423f
ianmllr <130236399+ianmllr@users.noreply.github.com>
2025-12-02 22:47:31 +0100

    2024 day one pt 2

--- src/main/java/advent2024/DayOne.java
+++ src/main/java/advent2024/DayOne.java
@@ -13,11 +13,10 @@ public class DayOne {
     public static void main() {
         Path path = Path.of(("src/main/resources/2024/day1input.txt"));
         List<String> parsedNumbers = splitNumbers(path);
-
         List<Integer> leftNumbers = new ArrayList<>();
         List<Integer> rightNumbers = new ArrayList<>();
-
         int totalDifference = 0;
+        int similarityScore = 0;
 
         for (int i = 0; i < Objects.requireNonNull(parsedNumbers).size(); i+= 2) {
             leftNumbers.add(Integer.parseInt(parsedNumbers.get(i)));
@@ -27,23 +26,30 @@ public class DayOne {
         Collections.sort(leftNumbers);
         Collections.sort(rightNumbers);
 
+        // part one
         for (int i = 0; i < leftNumbers.size(); i++) {
             int diff = Math.abs(leftNumbers.get(i) - rightNumbers.get(i));
             totalDifference += diff;
         }
 
+        // part two
+        for (Integer leftNumber : leftNumbers) {
+            int occurences = 0;
+            int currentLeftNumber = leftNumber;
+            for (Integer rightNumber : rightNumbers) {
+                if (rightNumber.equals(currentLeftNumber)) occurences++;
+            }
+            similarityScore += (currentLeftNumber * occurences);
+        }
         System.out.println(totalDifference);
-
+        System.out.println(similarityScore);
     }
-
-
-
 
     public static List<String> splitNumbers(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             List<String> allNumbers = new ArrayList<>();
-            String regex = "   ";
+            String regex = " {3}";
             while ((line = br.readLine()) != null) {
                 String[] parsedLine = line.split(regex);
                 allNumbers.addAll(List.of(parsedLine));
@@ -54,6 +60,4 @@ public class DayOne {
             return null;
         }
     }
-
-
 }
